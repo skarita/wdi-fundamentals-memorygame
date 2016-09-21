ae8046f271808b0fe91c4910ba95a92d3c2723f7
Shoshiro <skarita@gmail.com>
2016-09-21 11:49:57 +1000

    updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,38 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+}
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+}
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+}
+
+if (cardTwo === cardThree) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+}
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	alert("you found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
